2fca53662f90e0ab88257186a847f3ed65b967d9
Aristides Cruz <aristidescruz02@gmail.com>
2022-01-01 17:56:27 +0100

    Comple table with search filter

--- src/components/dataTable/DataTable.jsx
+++ src/components/dataTable/DataTable.jsx
@@ -1,10 +1,24 @@
-import "./dataTable.css";
 import MockData from "../../MOCK_DATA.json";
+import "./dataTable.css";
+import { useState } from "react";
 
 function DataTable() {
+  const [search, setSearch] = useState("");
+  console.log(search);
+
   return (
     <div className="data-table">
-      <div className="top-dataTable">{/* Search input and Buttons */}</div>
+      <div className="top-dataTable">
+        {/* Search input */}
+        <label>Alumnos</label>
+        <input
+          type="text"
+          placeholder="Buscar por Nombre, Email o Palabra clave..."
+          onChange={e => {
+            setSearch(e.target.value);
+          }}
+        />
+      </div>
       <div className="bottom-dataTable">
         <table>
           <thead>
@@ -18,16 +32,24 @@ function DataTable() {
             </tr>
           </thead>
           <tbody>
-            {MockData.map(m => (
+            {MockData.filter(val => {
+              if (search === "") {
+                return val;
+              } else if (
+                val.Nombre.toLowerCase().includes(search.toLowerCase()) ||
+                val.Email.toLowerCase().includes(search.toLowerCase()) ||
+                val.Etiquetas.toLowerCase().includes(search.toLowerCase())
+              ) {
+                return val;
+              }
+            }).map(m => (
               <tr key={m.id}>
                 <td>{m.Nombre}</td>
                 <td>{m.Ciudad}</td>
                 <td>{m.Pais}</td>
                 <td>{m.Telefono}</td>
                 <td>{m.Email}</td>
-                <td className="tag-container">
-                  <td className="tag"> {m.Etiquetas}</td>
-                </td>
+                <td className="tag-container">{m.Etiquetas}</td>
               </tr>
             ))}
           </tbody>
