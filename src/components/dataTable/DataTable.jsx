7c62acd9e3a2b6a9412d6822842023f647da3c14
Aristides Cruz <aristidescruz02@gmail.com>
2022-01-01 21:02:28 +0100

    Add sort funtionality

--- src/components/dataTable/DataTable.jsx
+++ src/components/dataTable/DataTable.jsx
@@ -3,8 +3,26 @@ import "./dataTable.css";
 import { useState } from "react";
 
 function DataTable() {
+  const [data, setData] = useState(MockData);
   const [search, setSearch] = useState("");
-  console.log(search);
+  const [order, setOrder] = useState("ASC");
+
+  const sorting = col => {
+    if (order === "ASC") {
+      const sorted = [...data].sort((a, b) =>
+        a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
+      );
+      setData(sorted);
+      setOrder("DSC");
+    }
+    if (order === "DSC") {
+      const sorted = [...data].sort((a, b) =>
+        a[col].toLowerCase() > b[col].toLowerCase() ? -1 : 1
+      );
+      setData(sorted);
+      setOrder("ASC");
+    }
+  };
 
   return (
     <div className="data-table">
@@ -20,38 +38,50 @@ function DataTable() {
         />
       </div>
       <div className="bottom-dataTable">
-        <table>
+        <table class="table table-sortable">
           <thead>
             <tr>
-              <th>Nombre</th>
-              <th>Ciudad</th>
-              <th>Pais</th>
+              <th onClick={() => sorting("Nombre")} className="filter">
+                Nombre <i class="fas fa-sort"></i>
+              </th>
+              <th onClick={() => sorting("Ciudad")} className="filter">
+                Ciudad <i class="fas fa-sort"></i>
+              </th>
+              <th onClick={() => sorting("Pais")} className="filter">
+                Pais <i class="fas fa-sort"></i>
+              </th>
               <th>Telefono</th>
-              <th>Email</th>
-              <th>Etiquetas</th>
+              <th onClick={() => sorting("Email")} className="filter">
+                Email <i class="fas fa-sort"></i>
+              </th>
+              <th onClick={() => sorting("Etiquetas")} className="filter">
+                Etiquetas <i class="fas fa-sort"></i>
+              </th>
             </tr>
           </thead>
           <tbody>
-            {MockData.filter(val => {
-              if (search === "") {
-                return val;
-              } else if (
-                val.Nombre.toLowerCase().includes(search.toLowerCase()) ||
-                val.Email.toLowerCase().includes(search.toLowerCase()) ||
-                val.Etiquetas.toLowerCase().includes(search.toLowerCase())
-              ) {
-                return val;
-              }
-            }).map(m => (
-              <tr key={m.id}>
-                <td>{m.Nombre}</td>
-                <td>{m.Ciudad}</td>
-                <td>{m.Pais}</td>
-                <td>{m.Telefono}</td>
-                <td>{m.Email}</td>
-                <td className="tag-container">{m.Etiquetas}</td>
-              </tr>
-            ))}
+            {data
+              .filter(val => {
+                if (search === "") {
+                  return val;
+                } else if (
+                  val.Nombre.toLowerCase().includes(search.toLowerCase()) ||
+                  val.Email.toLowerCase().includes(search.toLowerCase()) ||
+                  val.Etiquetas.toLowerCase().includes(search.toLowerCase())
+                ) {
+                  return val;
+                }
+              })
+              .map(m => (
+                <tr key={m.id}>
+                  <td>{m.Nombre}</td>
+                  <td>{m.Ciudad}</td>
+                  <td>{m.Pais}</td>
+                  <td>{m.Telefono}</td>
+                  <td>{m.Email}</td>
+                  <td className="tag-container">{m.Etiquetas}</td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
