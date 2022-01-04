d44bac2b3df17dec146b4498465e56ac6a6f770a
Aristides Cruz <aristidescruz02@gmail.com>
2022-01-04 23:22:21 +0100

    Css changes in the table container

---
+++ src/components/topSearch/TopSearch.jsx
@@ -0,0 +1,7 @@
+import "./topSearch.css";
+
+function TopSearch() {
+  return <div className="top-container">TopSearch</div>;
+}
+
+export default TopSearch;
--- src/pages/students/Students.jsx
+++ src/pages/students/Students.jsx
@@ -1,17 +1,19 @@
-import "./students.css"
-import SideNavBar from "../../components/sideNavBar/SideNavBar"
-import DataTable from "../../components/dataTable/DataTable"
+import "./students.css";
+import SideNavBar from "../../components/sideNavBar/SideNavBar";
+import DataTable from "../../components/dataTable/DataTable";
+import TopSearch from "../../components/topSearch/TopSearch";
 /***
- * This page is for the students 
+ * This page is for the students
  * we have the left menu, students table, right filter and upper search
  */
 function Students() {
-    return (
-        <div className="students-container">
-            <SideNavBar />
-            <DataTable/>
-        </div>
-    )
+  return (
+    <div className="students-container">
+      <SideNavBar />
+      <TopSearch />
+      <DataTable />
+    </div>
+  );
 }
 
-export default Students
+export default Students;
