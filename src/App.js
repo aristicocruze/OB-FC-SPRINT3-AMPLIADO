19b298182b1ca3396994ba7dbb6bfde4c15f7d84
Aristides Cruz <aristidescruz02@gmail.com>
2022-01-30 13:49:55 +0100

    Create context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "./context/Context";
 
 import Students from "./pages/students/Students";
 import SingleStudent from "./pages/singleStudent/SingleStudent";
 import LoginPage from "./components/login/LoginPage";
 
 function App() {
+  const { user } = useContext(Context);
+
   return (
     <div>
       <BrowserRouter>
